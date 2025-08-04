e77b60c1947010f0c5cc9d65afe1a125a1798f70
abulesh_mohd <abulesh.mohd999@gmail.com>
2025-08-04 12:41:51 +0530

    Checking after adding classes and some changes

--- src/modules/Game.java
+++ src/modules/Game.java
@@ -1,4 +1,12 @@
 package modules;
 
+import java.awt.*;
+
 public class Game {
+    private Board board;
+    private List<Player> players;
+    private int nextPlayerIndex;
+    private List<Move> moves;
+    private Player winner;
+    private GameState gameState;
 }
---
+++ src/modules/GameState.java
@@ -0,0 +1,8 @@
+package modules;
+
+public enum GameState {
+    NOT_STARTED,
+    IN_PROGRESS,
+    WIN,
+    DRAW
+}
